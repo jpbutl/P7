3b48ebabbe468c02458c9c970fabf1e2fbca7ef4
JULIAN BUTLER <jpbutler@10-150-44-141.dhcp-m.deakin.edu.au>
2017-05-12 20:38:48 +1000

    7th commit

--- Prac6/Prac6/Trip.swift
+++ Prac6/Prac6/Trip.swift
@@ -11,10 +11,15 @@ import UIKit
 
 class Trip
 {
+    
+    
     var tripDate : Date
     var tripDuration : Int
     var tripDestination : String
     var destinationImage : UIImage
+    
+    
+    
     
     init(date : Date, duration : Int, destination: String, img : UIImage)
     {
@@ -28,16 +33,58 @@ class Trip
 
 class Utilities
 {
+    
+    
     static var dateFormatter = DateFormatter()
     static var trips : [Trip] = []
+    
+    
+    func resizeImage(image: UIImage, newWidth: CGFloat) -> UIImage? {
+        
+        let scale = newWidth / image.size.width
+        let newHeight = image.size.height * scale
+        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
+        image.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
     static func loadTrips()
     {
         dateFormatter.dateFormat = "yy/mm/dd"
+        
+        //RESIZE IMAGES
+        
+        //var melbourneImg : UIImage
+        //melbourneImg = UIImage(named: "melbourne.jpg")!
+        
+        //melbourneImg = self.resizeImage(UIImage(named: "melbourne.jpg")!, 50.0)
+
+
+        // http://stackoverflow.com/questions/31966885/ios-swift-resize-image-to-200x200pt-px
+        
+        // Learn Swift: Scale Images Keep Aspect Ratio!
+
+        // https://www.youtube.com/watch?v=wXDkZqmXVBs
+        
+        // http://stackoverflow.com/questions/24795035/swift-extra-argument-in-call
+        
+        
+        // melbourne.jpg
+        // sydney.jpeg
+        
+        // melbourne10p.jpg  is 50% of 10%  //Redo with higher image quality
+        
         trips =
         [
-            Trip(date : dateFormatter.date(from: "17/04/24")!, duration : 2, destination : "Melbourne" , img : UIImage(named: "melbourne.jpg")!),
-            Trip(date : dateFormatter.date(from: "17/05/24")!, duration : 2, destination : "Sydney", img : UIImage(named: "sydney.jpeg")!)
+            Trip(date : dateFormatter.date(from: "17/04/24")!, duration : 2, destination : "Melbourne" , img : UIImage(named: "melbourne10p.jpg")!),
+            Trip(date : dateFormatter.date(from: "17/05/24")!, duration : 2, destination : "Sydney", img : UIImage(named: "sydney10p.jpeg")!)
         ]
         //refer to file as .jpg/.jpeg's
     }
 }
+
+
